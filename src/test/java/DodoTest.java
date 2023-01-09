95d797ceeca96ff5c459a7fdc76ea22893c6f156
Menkova Anastasiya <menkovaa17@gmail.com>
2023-01-09 20:27:59 +0300

    Change element by 4

--- src/test/java/DodoTest.java
+++ src/test/java/DodoTest.java
@@ -12,6 +12,6 @@ public class DodoTest {
     void addToCart() {
         open("https://dodopizza.ru/nizhnynovgorod");
         $(byText("Комбо")).click();
-        $(byText("3 пиццы")).click();
+        $(byText("4 пиццы")).click();
         }
 }
