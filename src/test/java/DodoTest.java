b485b74d946afd596645555de3a43a801097310b
Menkova Anastasiya <menkovaa@vtb.ru>
2023-01-09 19:32:13 +0300

    first commit

---
+++ src/test/java/DodoTest.java
@@ -0,0 +1,17 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DodoTest {
+    @Test
+    void addToCart() {
+        open("https://dodopizza.ru/nizhnynovgorod");
+        $(byText("Комбо")).click();
+        $(byText("2 пиццы")).click();
+        }
+}
